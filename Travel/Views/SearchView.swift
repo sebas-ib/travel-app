0cfef731154a9298b775ca77d3e993cdee8cddfd
sebas-ib <seb91423@gmail.com>
2024-10-13 22:01:25 -0700

    feat: add navigation
    Nav features still in progress

---
+++ Travel/Views/SearchView.swift
@@ -0,0 +1,23 @@
+//
+//  AccountView.swift
+//  Travel
+//
+//  Created by Sebastian Ibarra on 10/13/24.
+//
+
+import SwiftUI
+
+struct AccountView: View {
+    
+    var body: some View {
+        ZStack {
+            Color("BackgroundColor").ignoresSafeArea()
+            
+            Text("Account")
+        }
+    }
+}
+
+#Preview {
+    AccountView()
+}
